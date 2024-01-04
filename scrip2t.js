bd8f590f102f5780d6928afc7bb928e2b5e91cc3
Olga <olyaolenka@icloud.com>
2024-01-04 19:59:15 +0100

    first commit

---
+++ scrip2t.js
@@ -0,0 +1,27 @@
+//let number = 5;
+
+//number = 10;
+//console.log (number);
+//const obj = {
+//    a : 50
+//};
+//obj. a = 10;
+//console.log (obj) ;
+
+const numberOfFilms = +prompt ('Сколько фильмов Вы уже посмотрели?', '');
+const personalMovieDB =  {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt ('Один из последних просмотренных фильмов ?', ''),
+    b = prompt ('На сколько оцените его ?', ''),
+    c = prompt ('Один из последних просмотренных фильмов ?', ''),
+    d = prompt ('На сколько оцените его ?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;      
+console.log(personalMovieDB);  
+
--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let number = 5;
+//let number = 5;
 
-number = 10;
-console.log (number);
-const obj = {
-    a : 50
+//number = 10;
+//console.log (number);
+//const obj = {
+//    a : 50
+//};
+//obj. a = 10;
+//console.log (obj) ;
+
+const numberOfFilms = +promt ('Сколько фильмов Вы уже посмотрели?', '');
+const personalMovieDB =  {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-obj. a = 10;
-console.log (obj) ;
+const a = promt ('Один из последних просмотренных фильмов ?', ''),
+    b = promt ('На сколько оцените его ?', ''),
+    c = promt ('Один из последних просмотренных фильмов ?', ''),
+    d = promt ('На сколько оцените его ?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;      
+console.log(personalMovieDB);  
+
